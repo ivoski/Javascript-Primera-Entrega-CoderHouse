c280dbc27b7249943e9d25a14a687fc81a1215fc
Ivoski <skorskiivo@gmail.com>
2022-10-19 00:24:04 -0300

    Fin dia 18/10

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
-alert("Ebe is a most Gayfull boy in the mf world🤣.");
+//let nombreingresado = prompt ("Ingrese su nombre");
+//let apellidoingresado = prompt ("Ingrese su apellido");
+//let nombrecompleto = "Su nombre completo es: " + nombreingresado + " " + apellidoingresado;
+//alert(nombrecompleto);
+
+let nombreadmin = prompt ("Ingrese su nombre de administrador")
+
+if (nombreadmin == "Ivoski"){
+    alert("¡Hola, Ivoski!");
+}else{
+    alert("You not Ivoski, Get out here!");
+}
+
+for (let i = 1; i <= 8; i++) {
+    let ingresarNombre = prompt ("Ingresar Nombre");
+    alert("Turno N°"+i+"Nombre: "+ingresarNombre);
+}
+
+let total = 0
+
+function sumar (primernum, segnum){
+    total = primernum + segnum
+}
+
+function mostrar (mensaje) {
+    console.log (mensaje)
+}
+
+sumar(1000,500);
+mostrar(resultado);
