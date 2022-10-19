f4d15c0ed66eae98aeea53d05f788c3912b264be
Ivoski <skorskiivo@gmail.com>
2022-10-19 17:39:29 -0300

    Primera entrega Finalizada

--- js/main.js
+++ js/main.js
@@ -1,30 +1,21 @@
-//let nombreingresado = prompt ("Ingrese su nombre");
-//let apellidoingresado = prompt ("Ingrese su apellido");
-//let nombrecompleto = "Su nombre completo es: " + nombreingresado + " " + apellidoingresado;
-//alert(nombrecompleto);
 
-let nombreadmin = prompt ("Ingrese su nombre de administrador")
+function loginadmin(){
+    let nombreadmin = prompt ("Ingrese su nombre de administrador")
+    if (nombreadmin == "Ivoski"){
+        alert("¡Hola, Ivoski!");
+        sacarturno()
+    }else{
+        alert("You not Ivoski, Get out here!");
+    }
 
-if (nombreadmin == "Ivoski"){
-    alert("¡Hola, Ivoski!");
-}else{
-    alert("You not Ivoski, Get out here!");
 }
 
-for (let i = 1; i <= 8; i++) {
-    let ingresarNombre = prompt ("Ingresar Nombre");
-    alert("Turno N°"+i+"Nombre: "+ingresarNombre);
+function sacarturno(){
+    for (let i = 1; i <= 5; i++) {
+        let ingresarNombre = prompt ("Ingresar Nombre para sacar un turno");
+        alert("Turno N°"+i+"Nombre: "+ingresarNombre);
+    }
 }
 
-let total = 0
+loginadmin();
 
-function sumar (primernum, segnum){
-    total = primernum + segnum
-}
-
-function mostrar (mensaje) {
-    console.log (mensaje)
-}
-
-sumar(1000,500);
-mostrar(resultado);
