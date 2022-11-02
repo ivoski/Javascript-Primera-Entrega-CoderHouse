e5d74e19c2424066e50aa2883cc49ae733fd047f
Ivoski <skorskiivo@gmail.com>
2022-11-01 21:47:55 -0300

    subida inicial

--- js/main.js
+++ js/main.js
@@ -19,3 +19,23 @@ function sacarturno(){
 
 loginadmin();
 
+class Producto {
+    constructor(nombre, precio) {
+        this.nombre  = nombre.toUpperCase();
+        this.precio  = parseFloat(precio);
+        this.stock = false;
+    }
+    sumaIva() {
+        this.precio = this.precio * 1.21;
+    }
+}
+
+const productos = [];
+productos.push(new Producto("Shampoo", "370"));
+productos.push(new Producto("Cera para peinar", "400"));
+productos.push(new Producto("tintura", "700"));
+
+for (const producto of productos)
+    producto.sumaIva();
+
+    console.log(Producto.nombre)
